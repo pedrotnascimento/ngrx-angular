94607eac4569e0a2dd362361b64176fef2bf7275
pedrotnascimento@gmail.com <pedrotnascimento@gmail.com>
2023-07-26 23:02:03 -0300

    functionality of add,subtract and remove product using two-way data binding and template events, like I always  did

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+type Product = {
+  name: string;
+  quantity: number;
+  id: string;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-experiment';
+  product?: Product;
+
+  ngOnInit(): void {
+    this.product = {
+      name: "Ipsum Lorem",
+      quantity: 0,
+      id: "8e37fbf7-ed01-4ffc-9d8f-48a440d81ed3"
+    };
+  }
+
+  subtractProduct(product: Product) {
+    if (product.quantity <= 0) {
+      return;
+    }
+    product.quantity -= 1;
+  }
+
+  addProduct(product: Product) {
+    product.quantity += 1;
+  }
+
+  removeProduct(product: Product){
+    product.quantity = 0;
+  }
+
+  displayIfHasQuantity(product: Product) {
+    return { 'product-action': product.quantity > 0, 'product-disable': product.quantity <= 0 };
+  }
 }
